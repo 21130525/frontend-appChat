3013a3a997369d57bef46b30e4428fde3bde2ac2
Le Thanh Tam <21130525@st.hcmuaf.edu.vn>
2025-12-21 22:47:55 +0700

    feat: add redirect to login page in authentication routes

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -37,6 +37,7 @@ export const router = createBrowserRouter([
                 path: "auth",
                 element: <AuthLayout />,
                 children: [
+                    { index: true, element: <Navigate to="login" replace /> },
                     { path: "login", element: <LoginPage /> },
                     { path: "register", element: <RegisterPage /> },
                 ],
